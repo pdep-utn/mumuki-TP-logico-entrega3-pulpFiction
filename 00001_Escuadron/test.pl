af33c97305b517ec68f157c494fcc5b9fab05b79
fernando.dodino@gmail.com <fernando.dodino@gmail.com>
2016-05-02 16:05:13 -0400

    Mumuki Export on 2016-05-02 16:05:13 -0400

--- 00001_Escuadron/test.pl
+++ 00001_Escuadron/test.pl
@@ -1,2 +1,8 @@
 test('Escuadron incluye en algún momento a [marsellus, vincent, winston]', nondet):-
   escuadron([marsellus, vincent, winston]).
+  
+test('Vincent solo no forma un escuadron porque no llega a 15'):-
+  not(escuadron([vincent])).
+  
+test('Nunca se forma un escuadrón sin integrantes porque se llega a 15'):-
+  not(escuadron([])).  
