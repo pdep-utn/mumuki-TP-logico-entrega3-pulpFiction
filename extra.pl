04a10741becd4eb02ec4e932d0163ecf0e6c253f
fernando.dodino@gmail.com <fernando.dodino@gmail.com>
2016-05-02 16:07:14 -0400

    Mumuki Export on 2016-05-02 16:07:14 -0400

---
+++ extra.pl
@@ -0,0 +1,129 @@
+pareja(marsellus, mia).
+pareja(pumkin,    honeyBunny).
+
+% ACLARACION: Todos los puntos deben ser completamente inversibles
+
+% ***************************************************************************
+% Entrega 1: Relaciones entre individuos. Recursividad. Negación. 
+% ***************************************************************************
+
+% Punto 2
+% Necesitamos agregar la siguiente Información
+% Bernardo es pareja de Bianca y de Charo
+pareja(bernardo,    bianca).
+pareja(bernardo,    charo).
+
+% Punto 1
+% saleCon/2: determina si alguien está saliendo con otra persona, porque son pareja
+% ej. saleCon(mia, Quien) Quien = marsellus
+% La cláusula ¿es recursiva? En caso afirmativo separe caso base y caso recursivo.
+saleCon(Uno, Otro):-pareja(Uno, Otro).
+saleCon(Otro, Uno):-pareja(Uno, Otro).
+
+% No se sabe si Bianca es pareja de Demóstenes
+% No hacemos nada por principio de universo cerrado
+
+% trabajaPara(Empleador, Empleado)
+trabajaPara(marsellus, vincent).
+trabajaPara(marsellus, jules).
+trabajaPara(marsellus, winston).
+
+% Bernardo trabaja para cualquiera que trabaja para marsellus (salvo para jules)
+trabajaPara(Empleador, bernardo):-trabajaPara(marsellus, Empleador), Empleador \= jules.
+
+% George trabaja para todos los que salen con Bernardo
+trabajaPara(Empleador, george):-saleCon(Empleador, bernardo).
+
+% Punto 3
+% esFiel/1: una persona es fiel si no tiene más de una pareja
+% Alguien que no tiene pareja no puede ser fiel.
+% Resolverlo utilizando negación
+esFiel(Personaje):-saleCon(Personaje, Pareja), 
+    not((saleCon(Personaje, OtraPareja), Pareja \= OtraPareja)).
+
+% Punto 4
+% acataOrden/2
+% Alguien acata la orden de otra persona si esa persona trabaja para la otra 
+% directa o indirectamente (porque trabaja para otra persona que acata las
+% órdenes del segundo personaje)
+% ej.
+% La cláusula ¿es recursiva? En caso afirmativo separe caso base y caso recursivo.
+acataOrden(Empleado, Jefe):-trabajaPara(Empleado, Jefe).
+acataOrden(Empleado, Jefe):-trabajaPara(Empleado, OtroJefe),
+    acataOrden(OtroJefe, Jefe).
+
+% ***************************************************************************
+% Entrega 2: Polimorfismo. Functores. Aritmética. Predicados de orden superior.
+% ***************************************************************************
+% Agregamos la siguiente información de base
+% Información base
+personaje(pumkin,     ladron([licorerias, estacionesDeServicio])).
+personaje(honeyBunny, ladron([licorerias, estacionesDeServicio])).
+personaje(vincent,    mafioso(maton)).
+personaje(jules,      mafioso(maton)).
+personaje(marsellus,  mafioso(capo)).
+personaje(winston,    mafioso(resuelveProblemas)).
+personaje(mia,        actriz([foxForceFive])).
+personaje(butch,      boxeador).
+personaje(bernardo,   mafioso(cerebro)).
+personaje(bianca,     actriz([elPadrino1])).
+
+% También tenemos información de los encargos que le hacen los jefes a sus 
+% empleados, codificada en la base de la siguiente forma: 
+% encargo(Solicitante, Encargado, Tarea). 
+% las tareas pueden ser cuidar(Protegido), ayudar(Ayudado), buscar(Buscado, Lugar)
+encargo(marsellus, vincent,   cuidar(mia)).
+encargo(vincent,   elVendedor, cuidar(mia)).
+encargo(marsellus, winston, ayudar(jules)).
+encargo(marsellus, winston, ayudar(vincent)).
+encargo(marsellus, vincent, buscar(butch, losAngeles)).
+encargo(bernardo,  vincent, buscar(jules, fuerteApache)).
+encargo(bernardo,  winston, buscar(jules, sanMartin)).
+encargo(bernardo,  winston, buscar(jules, lugano)).
+
+% Punto 1
+% esPeligroso/1. Nos dice si un personaje es peligroso. Eso ocurre cuando:
+% realiza alguna actividad peligrosa: ser matón, o robar licorerías. 
+% o bien porque tiene empleados peligrosos
+esPeligroso(Personaje):-personaje(Personaje, mafioso(maton)).
+esPeligroso(Personaje):-personaje(Personaje, ladron(Cosas)), member(licorerias, Cosas).
+esPeligroso(Personaje):-trabajaPara(Empleado, Personaje), esPeligroso(Empleado).
+
+% Base de amigos
+amigo(vincent, jules).
+amigo(jules, jimmie).
+amigo(vincent, elVendedor).
+
+% Punto 2
+% sanCayetano/1: es quien a todos los que tiene cerca les da trabajo (algún encargo). 
+% Alguien tiene cerca a otro personaje si es su amigo o empleado. 
+tieneCerca(Personaje, Otro):-amigo(Personaje, Otro).
+tieneCerca(Personaje, Otro):-trabajaPara(Otro, Personaje).
+
+sanCayetano(Personaje):-tieneCerca(Personaje, _), 
+    forall(tieneCerca(Personaje, Alguien), encargo(Personaje, Alguien, _)).
+
+% Punto 3
+% personajesRespetables/1: genera la lista de todos los personajes respetables. 
+% Es respetable cuando su actividad tiene un nivel de respeto mayor a 9. Se sabe que:
+% - Las actrices tienen un nivel de respeto de la décima parte de su cantidad de peliculas.
+% - Los mafiosos que resuelven problemas tienen un nivel 10 de respeto,
+%    los matones 5 por cada encargo que tienen y los capos 20.
+% - Al resto no se les debe ningún nivel de respeto. 
+cantidadEncargos(Personaje, CantidadEncargos):-personaje(Personaje, _),
+    findall(Encargo, encargo(_, Personaje, Encargo), Encargos), 
+    length(Encargos, CantidadEncargos).
+
+nivelRespeto(Personaje, 20):-personaje(Personaje, mafioso(capo)).
+nivelRespeto(Personaje, NivelRespeto):-personaje(Personaje, mafioso(maton)),
+    cantidadEncargos(Personaje, CantidadEncargos), NivelRespeto is CantidadEncargos * 5.
+    % otra opción es reemplazar la línea de arriba por ...
+    % findall(5, encargo(_, Personaje, _), Encargos), sumlist(Encargos, NivelRespeto).
+    % fin
+nivelRespeto(Personaje, 10):-personaje(Personaje, mafioso(resuelveProblemas)).
+nivelRespeto(Personaje, Respeto):-personaje(Personaje, actriz(Peliculas)), 
+	length(Peliculas, Long),
+	Respeto is Long * 0.1.
+
+personajesRespetables(Personajes):-
+    findall(Personaje, (nivelRespeto(Personaje, NivelRespeto), NivelRespeto > 9), Personajes). 
