7d3b343cb01103781147b3a559cb76341d387a0d
fernando.dodino@gmail.com <fernando.dodino@gmail.com>
2016-05-04 15:50:12 -0400

    Mumuki Export on 2016-05-04 15:50:12 -0400

--- 00004_Suma personajes/test.pl
+++ 00004_Suma personajes/test.pl
@@ -1,2 +1,11 @@
 test('sumar los valores de una lista vacía no importa el criterio, siempre se relaciona con 0'):-
   sumaPersonajes([], _, 0).
+  
+test('el total de amigos de jimmie, jules y vincent es 3'):-
+  sumaPersonajes([jimmie, jules, vincent], cuantosAmigos, 3).
+
+test('marsellus, winston, mia, jimmie, jules y vincent tienen en total 7 encargos'):-
+  sumaPersonajes([marsellus, winston, mia, jimmie, jules, vincent], cantidadEncargos, 7).
+
+test('marsellus, winston, mia, jimmie, jules y vincent tienen en total 45.1 niveles de respeto'):-
+  sumaPersonajes([marsellus, winston, mia, jimmie, jules, vincent], nivelRespeto, 45.1).
