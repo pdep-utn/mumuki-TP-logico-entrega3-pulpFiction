c4892b7b604976720fd44f09a1a2b46351372967
lspigariol@gmail.com <lspigariol@gmail.com>
2016-05-19 11:20:38 -0400

    Mumuki Export on 2016-05-19 11:20:38 -0400

--- 00002_Batallon/test.pl
+++ 00002_Batallon/test.pl
@@ -1,11 +1,25 @@
-test('Batallón incluye en algún momento a [marsellus, vincent, winston]', nondet):-
-  batallon([marsellus, vincent, winston], 5).
-  
-test('Vincent y Winston forman parte del batallón de 5 encargos', nondet):-
+% no funciona bien
+%test('Batallón incluye en algún momento a [marsellus, vincent, winston]', nondet):-
+%  batallon([marsellus, vincent, winston], 5).
+
+test('Batallon de 5 encargos incluye a  marsellus, vincent y winston'):-
+  batallon(Batallon,5),
+  member( winston,Batallon),
+  member( vincent,Batallon),
+  member( marsellus,Batallon).
+
+test('Escuadron winston y vincent pero sin marsellus'):-
+  escuadron(Escuadron),
+  member( winston,Escuadron),
+  member( vincent,Escuadron),
+  not(member( marsellus,Escuadron)).
+
+test('Vincent y Winston forman parte del batallón de 5 encargos, sin marsellus', nondet):-
   batallon(Personajes, 5),
   member(winston, Personajes),
-  member(vincent, Personajes).
-  
+  member(vincent, Personajes),
+  not(member( marsellus,Personajes)).
+
 test('Vincent solo no forma un batallón porque no llega a 5 encargos'):-
   not(batallon([vincent], 5)).
   
