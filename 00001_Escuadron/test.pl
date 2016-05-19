7352b94b4add628ad0d5d191fc0dcda46b5331cb
lspigariol@gmail.com <lspigariol@gmail.com>
2016-05-19 11:08:34 -0400

    Mumuki Export on 2016-05-19 11:08:34 -0400

--- 00001_Escuadron/test.pl
+++ 00001_Escuadron/test.pl
@@ -1,5 +1,19 @@
-test('Escuadron incluye en algún momento a [marsellus, vincent, winston]', nondet):-
-  escuadron([marsellus, vincent, winston]).
+% no funciona bien
+%test('Escuadron incluye en algún momento a [marsellus, vincent, winston]', nondet):-
+%  escuadron([marsellus, vincent, winston]).
+
+test('Escuadron incluye a  [marsellus, vincent, winston]'):-
+  escuadron(Escuadron),
+  member( winston,Escuadron),
+  member( vincent,Escuadron),
+  member( marsellus,Escuadron).
+
+test('Escuadron con solo winston'):-
+  escuadron([winston]).
+  
+test('Escuadron con solo marsellus'):-
+  escuadron([marcellus]).
+  
   
 test('Vincent solo no forma un escuadron porque no llega a 15'):-
   not(escuadron([vincent])).
