924a0678cc1545943c2d1c3180c0687b7249ed37
occhiuzzi.santiago@gmail.com <occhiuzzi.santiago@gmail.com>
2016-06-25 21:05:58 -0400

    Mumuki Export on 2016-06-25 21:05:58 -0400

--- 00004_Suma personajes/test.pl
+++ 00004_Suma personajes/test.pl
@@ -1,8 +1,8 @@
 test('sumar los valores de una lista vacía no importa el criterio, siempre se relaciona con 0'):-
   sumaPersonajes([], _, 0).
   
-test('el total de amigos de jimmie, jules y vincent es 3'):-
-  sumaPersonajes([jimmie, jules, vincent], cuantosAmigos, 3).
+test('el total de amigos de jimmie, jules y vincent es 5'):-
+  sumaPersonajes([jimmie, jules, vincent], cuantosAmigos, 5).
 
 test('marsellus, winston, mia, jimmie, jules y vincent tienen en total 7 encargos'):-
   sumaPersonajes([marsellus, winston, mia, jimmie, jules, vincent], cantidadEncargos, 7).
